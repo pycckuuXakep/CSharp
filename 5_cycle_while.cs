91cf4ed77ba4fe2f839448c46607c9df00a3e55e
linuxsoid <113263435+pycckuuXakep@users.noreply.github.com>
2022-10-12 21:46:34 +0300

    Update 5_cycle_while.cs

--- 5_cycle_while.cs
+++ 5_cycle_while.cs
@@ -21,8 +21,7 @@ namespace Practice_6 {
         static void fourth() {
             int n = Convert.ToInt32(Console.ReadLine());
             int k = Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            while(++i*k <= n); i--;
+            int i = 0; while(++i*k <= n); i--;
             Console.WriteLine($"{n}/{k} = {i}\n{n}/{k} = {n-i*k}");
         }
 
