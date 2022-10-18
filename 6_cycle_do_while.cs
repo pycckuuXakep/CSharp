49a1239ce93aefba78ecc788bff9bacf2f256ff4
linuxsoid <113263435+pycckuuXakep@users.noreply.github.com>
2022-10-18 15:22:29 +0300

    Update 6_cycle_do_while.cs

--- 6_cycle_do_while.cs
+++ 6_cycle_do_while.cs
@@ -1,28 +1,35 @@
 static void first()
 {
+    Console.WriteLine("[FIRST]");
     Console.Write("The sum of first six's numbers is:");
     int i = 1;
     int sum = 0;
     do { sum += i++; } while (i <= 6);
     Console.WriteLine(sum);
+    Console.WriteLine("");
+
 }
 static void second()
 {
-    Console.WriteLine("Give me the first number: ");
+    Console.WriteLine("[SECOND]");
+    Console.WriteLine("Second exercise. I multiplicate numbers. Zero is end of file");
     int summarize = 1;
     int count = 0;
+    int x = 1;
     do
     {
         Console.WriteLine("Give me another num and I '*' it");
-        int x = Convert.ToInt32(Console.ReadLine());
         summarize *= x;
         count++;
+        x = Convert.ToInt32(Console.ReadLine());
     } while (x != 0);
     Console.WriteLine($"Sum of {count} num's is {summarize}");
+    Console.WriteLine("");
 }
 
 static void third()
 {
+    Console.WriteLine("[THIRD]");
     Console.WriteLine("Give me A and i give you the sum of 1 + 1/2 + 1/k where k is the smallest num and sum > A");
     int A = Convert.ToInt32(Console.ReadLine());
     double sum = 1;
@@ -34,10 +41,13 @@ static void third()
     } while (sum <= A);
     Console.WriteLine("The smallest k is " + k);
     Console.WriteLine("PS: sum is " + sum);
+    Console.WriteLine("");
+
 }
 
 static void fourth()
 {
+    Console.WriteLine("[FOURTH]");
     Console.WriteLine("Give me % (> 0 but < 25): ");
     double P = Convert.ToDouble(Console.ReadLine()) / 100;
     Console.WriteLine("THE P IS " + P);
@@ -50,4 +60,6 @@ static void fourth()
     } while (sum <= 11000);
     Console.WriteLine("MONTH: " + month);
     Console.WriteLine("VKLAD: " + sum);
+    Console.WriteLine("");
+
 }
