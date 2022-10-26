355f1916b85b312e0033241f20cf6f15a8a6ce7f
linuxsoid <113263435+pycckuuXakep@users.noreply.github.com>
2022-10-26 16:35:35 +0300

    Update 7_for_for_for.cs

--- 7_for_for_for.cs
+++ 7_for_for_for.cs
@@ -1,67 +1,67 @@
-        static void first()
-        {
-            Console.WriteLine("1^k + 2^k + n^k...");
-            Console.Write("Give N: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Give K: ");
-            int k = Convert.ToInt32(Console.ReadLine());
-            double sum = 0;
-            double num = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                for (int j = 1; j <= k; j++)
-                    sum += Math.Pow(i, j);
-            }
-            Console.WriteLine("SUM: " + sum);
-            Console.WriteLine();
-        }
+static void first()
+{
+    Console.WriteLine("1^k + 2^k + n^k...");
+    Console.Write("Give N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Give K: ");
+    int k = Convert.ToInt32(Console.ReadLine());
+    double sum = 0;
+    double num = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= k; j++)
+            sum += Math.Pow(i, j);
+    }
+    Console.WriteLine("SUM: " + sum);
+    Console.WriteLine();
+}
 
-        static void second()
-        {
-            Console.Write("Give me num of lines: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for(int i = 1; i <= n; i++)
-            {
-                for (int k = 1; k <= i; k++)
-                    Console.Write(i + " ");
-                Console.WriteLine();
-            }
-            Console.WriteLine();
+static void second()
+{
+    Console.Write("Give me num of lines: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    for(int i = 1; i <= n; i++)
+    {
+        for (int k = 1; k <= i; k++)
+            Console.Write(i + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 
-        }
+}
 
-        static void third()
-        {
-            for(int i = 1; i<=9; i++)
-            {
-                for (int k = 1; k <= 9; k++)
-                    Console.WriteLine($"{i} * {k} = {i * k}");
-                Console.WriteLine();
-            }
-            Console.WriteLine();
+static void third()
+{
+    for(int i = 1; i<=9; i++)
+    {
+        for (int k = 1; k <= 9; k++)
+            Console.WriteLine($"{i} * {k} = {i * k}");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 
-        }
+}
 
-        static void fourth()
-        {
-            Console.Write("Give me height of rect: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Give me width of rect: ");
-            int m = Convert.ToInt32(Console.ReadLine());
-            Console.Write("+");
-            for (int i = 1; i <= m; i++)
-                Console.Write("+");
-            Console.WriteLine("+");
-            for (int i = 1; i <= n; i++) {
-                Console.Write("+");
-                for (int k = 1; k <= m; k++)
-                    Console.Write("*");
-                Console.WriteLine("+");
-            }
-            Console.Write("+");
-            for (int i = 1; i <= m; i++)
-                Console.Write("+");
-            Console.WriteLine("+");
-            Console.WriteLine();
+static void fourth()
+{
+    Console.Write("Give me height of rect: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Give me width of rect: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("+");
+    for (int i = 1; i <= m; i++)
+        Console.Write("+");
+    Console.WriteLine("+");
+    for (int i = 1; i <= n; i++) {
+        Console.Write("+");
+        for (int k = 1; k <= m; k++)
+            Console.Write("*");
+        Console.WriteLine("+");
+    }
+    Console.Write("+");
+    for (int i = 1; i <= m; i++)
+        Console.Write("+");
+    Console.WriteLine("+");
+    Console.WriteLine();
 
-        }
+}
